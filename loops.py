79463776d0383f20114712d1230cc1452ca0d06a
anuj-kumar-tiwari68 <anujkumartiwari68@gmail.com>
2025-09-12 23:06:17 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,28 @@
+# for
+
+# for i in range(1,7,2):
+#     # print(i)
+
+# for i in range(1,11):
+#     # print(5 * i)
+
+# while
+# n = 1
+# num = 9
+# while n<=10:
+#     print(num * n)
+#     n += 1
+
+# while True:
+#     num1 = int(input("enter a number"))
+#     num2 = int(input("enter a number"))
+#     print(num1 + num2)
+#     repeat = input("do you want to repeat? (yes/no)")
+#     if repeat == "no":
+#         break
+
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
