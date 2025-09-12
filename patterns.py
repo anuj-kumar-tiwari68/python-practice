2369b9c9682652c15a877e31abefc8ad42c35e30
anuj-kumar-tiwari68 <anujkumartiwari68@gmail.com>
2025-09-13 02:15:11 +0530

    Patterns

---
+++ patterns.py
@@ -0,0 +1,14 @@
+# for i in range(1,11):
+#     for j in range(1,i+1):
+#         print(j,end = " ")
+#     print()
+
+# for i in range(1,11):
+#     for j in range(1,i+1):
+#         print(i,end = " ")
+#     print()
+
+for i in range(1,11):
+    for j in range(11,i,-1):
+        print(i,end = " ")
+    print()
