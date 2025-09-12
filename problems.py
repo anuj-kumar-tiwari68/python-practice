2b984b3b95e6eb96e30efcd55b1b400667136bba
anuj-kumar-tiwari68 <anujkumartiwari68@gmail.com>
2025-09-12 14:48:59 +0530

    problems

---
+++ problems.py
@@ -0,0 +1,40 @@
+name = "Anuj"
+age = 22
+address = "123 Main Street"
+# print(name)
+# print(age)
+# print(address)
+
+# method1
+a  = 10
+b  = 20
+c = a
+a = b
+b = c
+# print(a)
+# print(b)
+
+# method2
+a,b = b,a
+print(a)
+print(b)
+
+
+c = 2.3
+d = int(c)
+# print(d)
+
+# Name = input("Enter your name: ")
+# date_of_birth = input("Enter your date of birth: ")
+# father_name = input("Enter your father name: ")
+# mobile_no = int(input("Enter your mobile number: "))
+# class_name = input("Enter your class name: ")
+# print(Name)
+# print(date_of_birth)
+# print(father_name)
+# print(mobile_no)
+# print(class_name)
+
+# number = int(input("Enter a number: "))
+# fnumber = float(number)
+# print(fnumber)
