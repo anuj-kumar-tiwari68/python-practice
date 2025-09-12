f1b4f7c847faa79dcacc67a821ab13ee73470d91
anuj-kumar-tiwari68 <anujkumartiwari68@gmail.com>
2025-09-12 14:44:38 +0530

    strings

--- Strings.py
+++ Strings.py
@@ -1,4 +1,14 @@
-course = "I have to work hard"
-print(course[2])
-print(course[-2])
-print(course[-1:-5])
+# course = "I have to work hard"
+# print(course[2])
+# print(course[-2])
+# print(course[-1:-5])
+# print(course[1:])
+# print(course[:6])
+# print(course[1:-1])
+
+#+++++++++++++++++FORMATTED STRINGS+++++++++++++++++++++++++
+
+first = "John"
+last = "Smith"
+msg = f'{first} [{last}] is a coder '
+print(msg)
