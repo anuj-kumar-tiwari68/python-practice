e77a143b30fe799dfce0f801bd021f1f5c8558d4
anuj-kumar-tiwari68 <anujkumartiwari68@gmail.com>
2025-09-13 00:17:19 +0530

    loops continued

--- loops.py
+++ loops.py
@@ -22,7 +22,34 @@
 #         break
 
 
-for i in range(1,6):
-    for j in range(1,i+1):
-        print(j,end=" ")
-    print()
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+
+# for loop with conditional statements
+
+
+# for i in range(1,11):
+#     if i == 3:
+#         print("Add this song to favourites")
+#     else:
+#         print(i)
+
+# for i in range(1,101):
+#     if i % 8 == 0 and i % 12 == 0:
+#         print(i)
+#
+# break and continue
+
+# for i in range(1,11):
+#     if i == 5:
+#         continue
+#     else:
+#         print(i,end = " ")
+
+for i in range(1,11):
+    if i == 5:
+        break
+    else:
+        print(i,end = " ")
