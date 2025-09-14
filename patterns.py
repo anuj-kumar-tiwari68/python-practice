f9c60fc9e9863f26e55c62f33732e8e9f8a8a746
anuj-kumar-tiwari68 <anujkumartiwari68@gmail.com>
2025-09-14 16:42:53 +0530

    Patterns completed

--- patterns.py
+++ patterns.py
@@ -8,7 +8,27 @@
 #         print(i,end = " ")
 #     print()
 
+# for i in range(1,11):
+#     for j in range(11,i,-1):
+#         print(i,end = " ")
+#     print()
+
+# for i in range(1,11):
+#     for j in range(i,0,-1):
+#         print(j,end=" ")
+#     print()
+
+
+# for i in range(1,11):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print()
+# for i in range(11,1,-1):
+#     for k in range(1,i-1):
+#         print("*",end=" ")
+#     print()
+
 for i in range(1,11):
-    for j in range(11,i,-1):
-        print(i,end = " ")
+    for j in range(1,i+1):
+        print(i * j,end = " ")
     print()
